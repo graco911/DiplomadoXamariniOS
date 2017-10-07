884f2e0e4cabd1977350a60b4cab73d9fe7e5107
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2017-10-07 09:35:40 -0500

    SALLAB05
    Agregada verificacion de la actividad

--- PhoneApp/ViewController.cs
+++ PhoneApp/ViewController.cs
@@ -48,6 +48,23 @@ namespace PhoneApp
             };
         }
 
+        partial void VerifyButton_TouchUpInside(UIButton sender)
+        {
+            Validate();
+        }
+
+        async void Validate()
+        {
+            var Client = new SALLab05.ServiceClient();
+            var Result = await Client.ValidateAsync("correo", "contrasena", this);
+
+            var Alert = UIAlertController.Create("Resultado", $"{Result.Status}\n{Result.FullName}\n{Result.Token}",
+                                                 UIAlertControllerStyle.Alert);
+            Alert.AddAction(UIAlertAction.Create("Ok",
+                                                 UIAlertActionStyle.Default, null));
+            PresentViewController(Alert, true, null);
+        }
+
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
