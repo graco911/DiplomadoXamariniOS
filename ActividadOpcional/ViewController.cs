0f25b902518dffed9a8faae98910528e43331926
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2017-10-07 10:33:02 -0500

    Actividad Opcional
    Agregada la Actividad Opcional

---
+++ ActividadOpcional/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using UIKit;
+
+namespace ActividadOpcional
+{
+    public partial class ViewController : UIViewController
+    {
+        StringBuilder text = new StringBuilder();
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+            Button0.TouchUpInside += delegate { SetNumber("0"); };
+            Button1.TouchUpInside += delegate { SetNumber("1"); };
+            Button2.TouchUpInside += delegate { SetNumber("2"); };
+            Button3.TouchUpInside += delegate { SetNumber("3"); };
+            Button4.TouchUpInside += delegate { SetNumber("4"); };
+            Button5.TouchUpInside += delegate { SetNumber("5"); };
+            Button6.TouchUpInside += delegate { SetNumber("6"); };
+            Button7.TouchUpInside += delegate { SetNumber("7"); };
+            Button8.TouchUpInside += delegate { SetNumber("8"); };
+            Button9.TouchUpInside += delegate { SetNumber("9"); };
+        }
+
+        private void SetNumber(string number)
+        {
+            text.Append(number);
+            if (text.Length <= 9)
+            {
+                NumberLabel.Text = text.ToString();
+            }
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ ActividadOpcional/ViewController.designer.cs
@@ -0,0 +1,118 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace ActividadOpcional
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button0 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel NumberLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Button0 != null) {
+                Button0.Dispose ();
+                Button0 = null;
+            }
+
+            if (Button1 != null) {
+                Button1.Dispose ();
+                Button1 = null;
+            }
+
+            if (Button2 != null) {
+                Button2.Dispose ();
+                Button2 = null;
+            }
+
+            if (Button3 != null) {
+                Button3.Dispose ();
+                Button3 = null;
+            }
+
+            if (Button4 != null) {
+                Button4.Dispose ();
+                Button4 = null;
+            }
+
+            if (Button5 != null) {
+                Button5.Dispose ();
+                Button5 = null;
+            }
+
+            if (Button6 != null) {
+                Button6.Dispose ();
+                Button6 = null;
+            }
+
+            if (Button7 != null) {
+                Button7.Dispose ();
+                Button7 = null;
+            }
+
+            if (Button8 != null) {
+                Button8.Dispose ();
+                Button8 = null;
+            }
+
+            if (Button9 != null) {
+                Button9.Dispose ();
+                Button9 = null;
+            }
+
+            if (NumberLabel != null) {
+                NumberLabel.Dispose ();
+                NumberLabel = null;
+            }
+        }
+    }
+}
